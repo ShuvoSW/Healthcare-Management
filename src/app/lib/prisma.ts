9c638a766069df2fc1c95a02d77d09cd468997d8
ShuvoSW <shuvomajumder3369@gmail.com>
2026-02-11 22:51:43 +0600

    Creating Zod Middleware And Creating Zod Error Handler For Global Error Handler

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "../../generated/prisma/client"
-import { envVars } from "../../config/env";
+import { envVars } from "../config/env";
 
 // const connectionString = `${process.env.DATABASE_URL}`
 const connectionString = envVars.DATABASE_URL
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import { env } from "node:process";
-import { envVars } from "../../config/env";
+import { envVars } from "../config/env";
 import status from "http-status";
+import z from "zod";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import { handleZodError } from "../errorHelpers/handleZodError";
+
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const globalErrorHandler = (err:any, req: Request, res: Response, next: NextFunction) => {
@@ -10,12 +14,58 @@ export const globalErrorHandler = (err:any, req: Request, res: Response, next: N
     console.error("Error from Global Error Handler", err);
   }
 
-  const statusCode : number = status.INTERNAL_SERVER_ERROR;
-  const message : string = 'Internal Server Error';
+  let errorSources: TErrorSources[] = []
+  let statusCode : number = status.INTERNAL_SERVER_ERROR;
+  let message : string = 'Internal Server Error';
 
-  res.status(statusCode).json({
+  // Zod error pattern
+  /*
+  error.issues; 
+      [
+      {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' ],
+        message: 'Invalid input: expected string'
+      },
+      {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+      }
+    ]
+      */
+
+  if (err instanceof z.ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode as number;
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+
+  //   err.issues.forEach(issue => {
+  //   errorSources.push({
+  //     path: issue.path.join(" => "), 
+  //     message: issue.message
+  //   })
+  // })
+  }
+
+  
+
+  const errorResponse: TErrorResponse ={
     success: false,
     message: message,
-    error: err.message,
-  })
+    errorSources,
+    error: envVars.NODE_ENV === 'development' ? err: undefined,
+    
+  }
+  // res.status(statusCode).json({
+  //   success: false,
+  //   message: message,
+  //   errorSources,
+  //   error: envVars.NODE_ENV === 'development' ? err: undefined,
+    
+  // })
+  res.status(statusCode).json(errorResponse)
 }
