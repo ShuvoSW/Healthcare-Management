9cc8fb5f01654d8a619264a2893478be64a63148
ShuvoSW <shuvomajumder3369@gmail.com>
2026-03-17 22:08:51 +0600

    Update My Profile Route For Patient Role Part-2

---
+++ src/app/module/patient/patient.interface.ts
@@ -0,0 +1,39 @@
+import { BloodGroup, Gender } from "../../../generated/prisma/enums";
+
+export interface IUpdatePatientInfoPayload {
+    name: string;
+    profilePhoto: string;
+    contactNumber: string;
+    address: string
+}
+export interface IUpdatePatientHealthDataPayload {
+    gender: Gender;
+    dateOfBirth: Date;
+    bloodGroup: BloodGroup;
+    hasAllergies: boolean;  
+    hasDiabetes: boolean;
+    height: string;
+    weight: string;
+    smokingStatus: boolean;
+    dietaryPreferences: string;
+    pregnancyStatus: boolean;
+    mentalHealthHistory: string;
+    immunizationStatus: string;
+    hasPastSurgeries: boolean;
+    recentAnxiety: boolean;
+    recentDepression: boolean;
+    maritalStatus: string;
+}
+
+export interface IUpdatePatientMedicalReportPayload {
+    reportName: string;
+    reportLing: string;
+    shouldDelete: boolean;
+    reportId: string;
+}
+
+export interface IUpdatePatientProfilePayload {
+    patientInfo: IUpdatePatientInfoPayload;
+    patientHealthData:IUpdatePatientHealthDataPayload;
+    medicalReports: IUpdatePatientMedicalReportPayload;
+}
