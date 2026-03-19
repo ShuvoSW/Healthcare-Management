912e8035223a2172336c2a44ddc2cf51abc808cb
ShuvoSW <shuvomajumder3369@gmail.com>
2026-03-19 19:04:10 +0600

    Creating Dashboard API

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,6 +9,8 @@ import { AppointmentRoutes } from "../module/appointment/appointment.route";
 import { PatientRoutes } from "../module/patient/patient.route";
 import { PrescriptionRoutes } from "../module/prescription/prescription.route";
 import { ReviewRoutes } from "../module/review/review.route";
+import { StatsRoutes } from "../module/stats/stats.route";
+import { PaymentRoutes } from "../module/payment/payment.route";
 
 
 const router = Router();
@@ -23,5 +25,7 @@ router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use("/appointments", AppointmentRoutes)
 router.use("/prescriptions", PrescriptionRoutes)
 router.use("/reviews", ReviewRoutes)
+router.use("/stats", StatsRoutes)
+router.use("/payments",PaymentRoutes)
 
 export const IndexRoutes = router;  
